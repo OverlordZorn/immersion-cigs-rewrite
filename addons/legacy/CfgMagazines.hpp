8fb4ced3a7783bd17ec5895606a1e7e551c9e692
OverlordZorn <dennisAzorn@gmail.com>
2024-12-04 19:27:31 +0100

    Create CfgMagazines.hpp

---
+++ addons/legacy/CfgMagazines.hpp
@@ -0,0 +1,22 @@
+class CfgMagazines {
+    class PVAR(lighter);
+    class murshun_cigs_lighter : PVAR(lighter)
+    {
+        scope = 2;
+        displayName = "DEPRECATED";
+    };
+
+    class PVAR(matches);
+    class murshun_cigs_matches : PVAR(matches)
+    {
+        scope = 2;
+        displayName = "DEPRECATED";
+    };
+
+    class EGVAR(morley,cigpack);
+    class murshun_cigs_cigpack : EGVAR(morley,cigpack)
+    {
+        scope = 2;
+        displayName = "DEPRECATED";
+    };
+};
