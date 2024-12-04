824a2796e38460a99c6cf06895112457d3597fec
OverlordZorn <dennisAzorn@gmail.com>
2024-12-04 19:28:13 +0100

    Update script_macros.hpp

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,3 +1,10 @@
 #include "\x\cba\addons\main\script_macros.hpp"
 #include "script_macros_zrn.hpp"
 
+// Mod Specific Macros
+
+#ifdef __A3_DEBUG__
+    #define _DEBUG_SCOPE_ 2
+#else
+    #define _DEBUG_SCOPE_ 1
+#endif
