78df5f9d24490bc2c974c31e095e23d66d890294
OverlordZorn <dennisAzorn@gmail.com>
2024-12-04 19:27:02 +0100

    Delete script_component.hpp

--- addons/compat_murshun/script_component.hpp
+++
@@ -1,8 +0,0 @@
-#define COMPONENT compat_murshun
-
-// This is being used for the Addon's Name and can be "My Addon Template Framework"
-#define COMPONENT_BEAUTIFIED Compat for Old Classnames
-
-
-#include "\z\cigs\addons\main\script_mod.hpp"
-#include "\z\cigs\addons\main\script_macros.hpp"
