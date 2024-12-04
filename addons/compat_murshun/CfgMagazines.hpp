b69bd380bc459a91288260492aeb2ed1b95f3f7b
OverlordZorn <dennisAzorn@gmail.com>
2024-12-04 15:28:16 +0100

    Update CfgMagazines.hpp

--- addons/compat_murshun/CfgMagazines.hpp
+++ addons/compat_murshun/CfgMagazines.hpp
@@ -2,7 +2,7 @@ class CfgMagazines {
     class PVAR(lighter);
     class murshun_cigs_lighter : PVAR(lighter)
     {
-        scope = 1;
+        scope = 2;
         displayName = "DEPRECATED";
         PVAR(replacementItems) = QPVAR(lighter);
     };
@@ -10,7 +10,7 @@ class CfgMagazines {
     class PVAR(matches);
     class murshun_cigs_matches : PVAR(matches)
     {
-        scope = 1;
+        scope = 2;
         displayName = "DEPRECATED";
         PVAR(replacementItems) = QPVAR(matches);
     };
@@ -18,7 +18,7 @@ class CfgMagazines {
     class EGVAR(morley,cigpack);
     class murshun_cigs_cigpack : EGVAR(morley,cigpack)
     {
-        scope = 1;
+        scope = 2;
         displayName = "DEPRECATED";
         PVAR(replacementItems) = QEGVAR(morley,cigpack);
     };
