03c5de47397fb513a8a24b54f5839be524434928
OverlordZorn <dennisAzorn@gmail.com>
2024-12-04 14:49:28 +0100

    Update CfgWeapons.hpp

--- addons/compat_murshun/CfgWeapons.hpp
+++ addons/compat_murshun/CfgWeapons.hpp
@@ -8,30 +8,35 @@ class CfgWeapons {
     class immersion_cigs_cigar0_nv: EGVAR(cigars,cigar0_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(cigars,cigar0_nv);
     };
 
     class EGVAR(cigars,cigar1_nv);
     class immersion_cigs_cigar1_nv: EGVAR(cigars,cigar1_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(cigars,cigar1_nv);
     };
 
     class EGVAR(cigars,cigar2_nv);
     class immersion_cigs_cigar2_nv: EGVAR(cigars,cigar2_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(cigars,cigar2_nv);
     };
 
     class EGVAR(cigars,cigar3_nv);
     class immersion_cigs_cigar3_nv: EGVAR(cigars,cigar3_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(cigars,cigar3_nv);
     };
 
     class EGVAR(cigars,cigar4_nv);
     class immersion_cigs_cigar4_nv: EGVAR(cigars,cigar4_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(cigars,cigar4_nv);
     };
 
     // Cigarettes
@@ -39,30 +44,35 @@ class CfgWeapons {
     class murshun_cigs_cig0_nv: EGVAR(morley,cig0_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(morley,cig0_nv);
     };
 
     class EGVAR(morley,cig1_nv);
     class murshun_cigs_cig1_nv: EGVAR(morley,cig1_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(morley,cig1_nv);
     };
 
     class EGVAR(morley,cig2_nv);
     class murshun_cigs_cig2_nv: EGVAR(morley,cig2_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(morley,cig2_nv);
     };
 
     class EGVAR(morley,cig3_nv);
     class murshun_cigs_cig3_nv: EGVAR(morley,cig3_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(morley,cig3_nv);
     };
 
     class EGVAR(morley,cig4_nv);
     class murshun_cigs_cig4_nv: EGVAR(morley,cig4_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(morley,cig4_nv);
     };
 
     // Pops
@@ -70,5 +80,6 @@ class CfgWeapons {
     class immersion_pops_pop0_nv: EGVAR(pops,pop0_nv) {
         scope = 1;
         displayName = "DEPRECATED";
+        PVAR(replacementItems) = QEGVAR(pops,pop0_nv);
     };
 };
