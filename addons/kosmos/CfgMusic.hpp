c6c1b14e4ce3ae1738557cb077336f1048cb00cf
OverlordZorn <dennisAzorn@gmail.com>
2025-02-16 00:01:25 +0100

    change file names

--- addons/kosmos/CfgMusic.hpp
+++ addons/kosmos/CfgMusic.hpp
@@ -3,42 +3,42 @@ class CfgMusic
     class kino_blood_type
     {
         name = "Kino - Группа крови";
-        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - Группа крови.ogg), "db + 0" , 1.0 };
+        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - blood_type.ogg), "db + 0" , 1.0 };
         musicClass = "cigs_kino";
         duration = 235;
     };
     class kino_im_declaring_my_house
     {
         name = "Kino - Звезда по имени Солнце";
-        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - Звезда по имени Солнце.ogg), "db + 0" , 1.0 };
+        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - im_declaring_my_house.ogg), "db + 0" , 1.0 };
         musicClass = "cigs_kino";
         duration = 225;
     };
     class kino_kukushka
     {
         name = "Kino - Кукушка";
-        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - Кукушка.ogg), "db + 0" , 1.0 };
+        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - kukushka.ogg), "db + 0" , 1.0 };
         musicClass = "cigs_kino";
         duration = 638;
     };
     class kino_leto
     {
         name = "Kino - Лето";
-        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - Лето.ogg), "db + 0" , 1.0 };
+        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - leto.ogg), "db + 0" , 1.0 };
         musicClass = "cigs_kino";
         duration = 594;
     };
     class kino_goodnight
     {
         name = "Kino - Спокойная ночь";
-        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - Спокойная ночь.ogg), "db + 0" , 1.0 };
+        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - goodnight.ogg), "db + 0" , 1.0 };
         musicClass = "cigs_kino";
         duration = 622;
     };
     class kino_changement
     {
         name = "Kino - Хочу перемен";
-        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - Хочу перемен.ogg), "db + 0" , 1.0 };
+        sound[] = {PATH_TO_ADDON_3(data,ogg,Kino - changement.ogg), "db + 0" , 1.0 };
         musicClass = "cigs_kino";
         duration = 532;
     };
