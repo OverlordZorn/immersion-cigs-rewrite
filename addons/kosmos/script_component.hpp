e447d8e2e152dbd0aa55e8ffbca4a392381f7b19
OverlordZorn <dennisAzorn@gmail.com>
2025-02-15 16:17:09 +0100

    init

---
+++ addons/kosmos/script_component.hpp
@@ -0,0 +1,8 @@
+#define COMPONENT Kosmos
+
+// This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED Kosmos Cigarettes
+
+
+#include "\z\cigs\addons\main\script_mod.hpp"
+#include "\z\cigs\addons\main\script_macros.hpp"
